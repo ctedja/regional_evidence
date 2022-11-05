015b8d34f64faea8d0968b990956d925af3a9d3d
Clinton Tedja <clintontedja@gmail.com>
2022-11-05 12:11:53 +0700

    update

--- script.py
+++ script.py
@@ -13,9 +13,15 @@ evidence_dataset.info()
 evidence_dataset = pd.DataFrame(evidence_dataset)
 evidence_dataset = evidence_dataset.filter(['Country', 'Title', 'Category', 'Date', 'Link', 'Image'])
 evidence_dataset['Date'] = evidence_dataset['Date'].dt.date
+evidence_dataset = evidence_dataset.sort_values(by=['Date', 'Country'], ascending=False)
 evidence_dataset['Image'] = evidence_dataset['Image'].fillna('https://raw.githubusercontent.com/ctedja/evidence_app/main/static/images/empty.png')
 evidence_dataset = evidence_dataset.fillna("")
-evidence_dataset.to_json(path_or_buf="data.json", orient='values')
+evidence_dataset.to_json(path_or_buf="data.json", orient='records')
+evidence_dataset.to_json(path_or_buf="data2.json", orient='values')
+
+d = {"data": evidence_dataset.to_dict('records')}
+with open("data.json", "w") as f:
+    json.dump(d, f, default=str)
 
 #evidence_dataset = evidence_dataset.to_json(orient='values')
 evidence_dataset = evidence_dataset.values.tolist()
