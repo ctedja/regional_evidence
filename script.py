682d2b377d1678aee712a29127379fee8b6cf678
Clinton Tedja <clintontedja@gmail.com>
2022-11-06 23:49:04 +0700

    update, all working
    A few little things need to be solved, like how the filters act when you remove a filter

--- script.py
+++ script.py
@@ -12,12 +12,13 @@ evidence_dataset = pd.read_excel('evidence_dataset.xlsx')
 evidence_dataset.info()
 evidence_dataset = pd.DataFrame(evidence_dataset)
 evidence_dataset = evidence_dataset.filter(['Country', 'Title', 'Category', 'Date', 'Link', 'Image'])
+evidence_dataset = evidence_dataset.drop_duplicates(keep='first')
 evidence_dataset['Date'] = evidence_dataset['Date'].dt.date
-evidence_dataset = evidence_dataset.sort_values(by=['Date', 'Country'], ascending=False)
+evidence_dataset = evidence_dataset.sort_values(by=['Date', 'Country'])
 evidence_dataset['Image'] = evidence_dataset['Image'].fillna('https://raw.githubusercontent.com/ctedja/evidence_app/main/static/images/empty.png')
 evidence_dataset = evidence_dataset.fillna("")
-evidence_dataset.to_json(path_or_buf="data.json", orient='records')
-evidence_dataset.to_json(path_or_buf="data2.json", orient='values')
+# evidence_dataset.to_json(path_or_buf="data.json", orient='records')
+# evidence_dataset.to_json(path_or_buf="data2.json", orient='values')
 
 d = {"data": evidence_dataset.to_dict('records')}
 with open("data.json", "w") as f:
